6a2a5a391c88739fcf0a68684a4226c2ec7b4f16
Akinaru <maxime.utchimata@gmail.com>
2025-02-08 15:05:51 +0100

    ADD [options] Dialogue for options

---
+++ src/dialogues/options.ts
@@ -0,0 +1,13 @@
+export const optionsDialogue = {
+    "start": {
+      text: "Il n'y a rien pour le moment dans cette section :(",
+      responses: [],
+      autoNext: "encouragement"
+    },
+    "encouragement": {
+      text: "Mais ne t'inquiète pas, le contenu arrive bientôt ! *range ses notes avec enthousiasme*",
+      responses: []
+    }
+};
+
+export default optionsDialogue;
